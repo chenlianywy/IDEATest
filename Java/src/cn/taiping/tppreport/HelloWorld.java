61a27b678af556235b5c954274d1c2b52856de87
chenlian <chenlianywy@163com>
2020-12-23 18:02:34 +0800

    提交代码

---
+++ Java/src/cn/taiping/tppreport/HelloWorld.java
@@ -0,0 +1,13 @@
+package cn.taiping.tppreport;
+
+
+
+/**
+ * @author chenla
+ * @create 2020-12-23 5:57 PM
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
