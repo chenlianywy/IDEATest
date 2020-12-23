25dbeac01fa8dfa6834a824ed522e31d44c180d5
chenlian <chenlianywy@163com>
2020-12-23 18:05:13 +0800

    提交代码，修改

--- Java/src/cn/taiping/tppreport/HelloWorld.java
+++ Java/src/cn/taiping/tppreport/HelloWorld.java
@@ -9,5 +9,7 @@ package cn.taiping.tppreport;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world!!!");
+        System.out.println("hello world!!!");
+        System.out.println("hello world!!!");
     }
 }
